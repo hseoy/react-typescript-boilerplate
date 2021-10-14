a537b7560630a384f80d1c1ab078759a883152cd
Yunseo Hwang <yunseo.dev@gmail.com>
2021-10-14 15:30:57 +0900

    chore: html-webpack-plugin 추가

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const imageInlineSizeLimit = process.env.IMAGE_INLINE_SIZE_LIMIT
   ? parseInt(process.env.IMAGE_INLINE_SIZE_LIMIT)
@@ -15,7 +16,8 @@ module.exports = {
     rules: [
       {
         test: /\.(js|jsx|ts|tsx)$/,
-        use: ['babel-loader', 'ts-loader']
+        use: ['babel-loader', 'ts-loader'],
+        exclude: /node_modules/,
       },
       {
         test: /\.(bmp|gif|jpe?g|png|svg)$/i,
@@ -26,6 +28,12 @@ module.exports = {
       },
     ]
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html',
+    }),
+  ],
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
   },
