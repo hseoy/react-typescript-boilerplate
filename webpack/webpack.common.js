3b3ef4571c88cdc1446f423d4202d1ebbfe86825
Yunseo Hwang <yunseo.dev@gmail.com>
2021-10-14 12:00:09 +0900

    chore: 이미지를 불러오기 위한 webpack 설정 추가

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -2,6 +2,10 @@
 
 const path = require('path');
 
+const imageInlineSizeLimit = process.env.IMAGE_INLINE_SIZE_LIMIT
+  ? parseInt(process.env.IMAGE_INLINE_SIZE_LIMIT)
+  : 1024 * 10;
+
 module.exports = {
   entry: './src/index.tsx',
   output: {
@@ -12,7 +16,14 @@ module.exports = {
       {
         test: /\.(js|jsx|ts|tsx)$/,
         use: ['babel-loader', 'ts-loader']
-      }
+      },
+      {
+        test: /\.(bmp|gif|jpe?g|png|svg)$/i,
+        loader: 'url-loader',
+        options: {
+          limit: imageInlineSizeLimit,
+        },
+      },
     ]
   },
   resolve: {
