ab8314cfe36f66bdcba478005aef96069fa219a6
Yunseo Hwang <yunseo.dev@gmail.com>
2021-10-14 15:59:13 +0900

    chore: css 파일 경로 지정 설정 추가

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -19,6 +19,8 @@ module.exports = {
     ],
   },
   plugins: [
-    new MiniCssExtractPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'static/css/[name].[contenthash:8].css',
+    }),
   ],
 };
