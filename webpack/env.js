5a04f9a405654aa3a145d1d303e4892b85bcd147
Yunseo Hwang <yunseo.dev@gmail.com>
2021-10-17 13:58:03 +0900

    chore: DefinePlugin에서 NODE_ENV 삭제

--- webpack/env.js
+++ webpack/env.js
@@ -8,9 +8,7 @@ function getClientEnvironment() {
     .reduce((env, key) => {
       env[key] = process.env[key];
       return env;
-    }, {
-      NODE_ENV: process.env.NODE_ENV || 'development',
-    });
+    }, {});
   const stringified = {
     'process.env': Object.keys(raw).reduce((env, key) => {
       env[key] = JSON.stringify(raw[key]);
