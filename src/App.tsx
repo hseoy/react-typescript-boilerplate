d37f32fc253ed7e8ea0f7c6013520dd82e20586c
Yunseo Hwang <yunseo.dev@gmail.com>
2021-10-17 14:06:18 +0900

    chore: env-cmd 추가

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import testImage from "./assets/images/test.jpg";
 const App: FC = () => {
   return (
     <>
-      <h1>Hello World {process.env.NODE_ENV}</h1>
+      <h1>Hello World {process.env.APP_ENV}</h1>
       <img src={testImage} width="500px" />
     </>
   );
